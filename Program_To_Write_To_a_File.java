20602fd4501bfaaed6ddbb38d38c7852b3dc9219
somyx10 <141742051+somyx10@users.noreply.github.com>
2023-08-15 22:03:43 +0530

    Create Program_To_Write_To_a_File.java

---
+++ Program_To_Write_To_a_File.java
@@ -0,0 +1,21 @@
+import java.io.FileWriter;
+ 
+import java.io.IOException;
+ 
+public class ABC {
+    public static void main(String[] args)
+    {
+        try {
+            FileWriter Writer
+                = new FileWriter("myfile.txt");
+            Writer.write(
+                "Files in Java are seriously good!!");
+            Writer.close();
+            System.out.println("Successfully written.");
+        }
+        catch (IOException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
