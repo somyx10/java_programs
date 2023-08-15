beb8e0d8fcd155b1fe307dd6d0975645a1c36432
somyx10 <141742051+somyx10@users.noreply.github.com>
2023-08-15 22:02:13 +0530

    Create Program_to_Read_from_a_file.java

---
+++ Program_to_Read_from_a_file.java
@@ -0,0 +1,24 @@
+import java.io.File;
+ 
+import java.io.FileNotFoundException;
+ 
+import java.util.Scanner;
+ 
+public class GFG {
+    public static void main(String[] args)
+    {
+        try {
+            File Obj = new File("myfile.txt");
+            Scanner Reader = new Scanner(Obj);
+            while (Reader.hasNextLine()) {
+                String data = Reader.nextLine();
+                System.out.println(data);
+            }
+            Reader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error has occurred.");
+            e.printStackTrace();
+        }
+    }
+}
