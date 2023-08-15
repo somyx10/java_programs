9c35f862827de35b2d5157a70fb2393b952ef33b
somyx10 <141742051+somyx10@users.noreply.github.com>
2023-08-15 22:04:54 +0530

    Create Program_To_Delete_a_File.java

---
+++ Program_To_Delete_a_File.java
@@ -0,0 +1,16 @@
+import java.io.File;
+ 
+public class ABC {
+    public static void main(String[] args)
+    {
+        File Obj = new File("myfile.txt");
+        if (Obj.delete()) {
+            System.out.println("The deleted file is : "
+                               + Obj.getName());
+        }
+        else {
+            System.out.println(
+                "Failed in deleting the file.");
+        }
+    }
+}
