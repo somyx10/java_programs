8432195b2412f6d9a4312f965f458b1b69894d21
somyx10 <141742051+somyx10@users.noreply.github.com>
2023-08-15 21:52:09 +0530

    Create  Program_For_Method_Overloading_by_Using_Different_Numbers_of_Arguments.java

---
+++ Program_For_Method_Overloading_by_Using_Different_Numbers_of_Arguments.java
@@ -0,0 +1,23 @@
+class Helper {
+ 
+    static int Multiply(int a, int b)
+    {
+ 
+        return a * b;
+    }
+ 
+    static int Multiply(int a, int b, int c)
+}
+{  
+return a * b * c;
+    }
+ 
+class GFG {
+ 
+    public static void main(String[] args)
+    {
+
+        System.out.println(Helper.Multiply(2, 4));
+        System.out.println(Helper.Multiply(2, 7, 3));
+    }
+}
