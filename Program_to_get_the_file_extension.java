43f193728358da93bc91026fd722eceb5e87f62b
somyx10 <141742051+somyx10@users.noreply.github.com>
2023-08-17 09:08:02 +0530

    Create Program_to_get_the_file_extension.java

---
+++ Program_to_get_the_file_extension.java
@@ -0,0 +1,16 @@
+import java.io.File;
+
+class Main {
+
+  public static void main(String[] args) {
+    File file = new File("Test.java");
+
+    String fileName = file.toString();
+
+    int index = fileName.lastIndexOf('.');
+    if(index > 0) {
+      String extension = fileName.substring(index + 1);
+      System.out.println("File extension is " + extension);
+    }
+  }
+}
